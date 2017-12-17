b1f00ee9762b6e43281b5f2948703460259a7c75
Fabrice Manhart <fabrice.manhart@zuehlke.com>
2017-12-17 15:00:13 +0100

    project history

--- src/app/employees/employee/project-history/project-history.component.ts
+++ src/app/employees/employee/project-history/project-history.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { ProjectParticipation } from '../../../core/models/project-participation';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { DataService } from './../../../core/services/data.service';
 
 @Component({
   selector: 'app-project-history',
   templateUrl: './project-history.component.html',
   styleUrls: ['./project-history.component.scss']
 })
-export class ProjectHistoryComponent implements OnInit {
+export class ProjectHistoryComponent {
+  url: string;
+  code: string;
+  projects: Array<ProjectParticipation>;
 
-  constructor() { }
+  constructor(private dataService: DataService, route: ActivatedRoute) {
+    route.paramMap.subscribe(p => {
+      this.code = p.get('code');
+      this.url = 'http://localhost:41588/api/v1/employees/' + this.code;
 
-  ngOnInit() {
+      dataService
+        .getAll<Array<ProjectParticipation>>(this.url + '/projects/history')
+        .subscribe(res => (this.projects = res));
+
+      return [];
+    });
   }
-
 }
