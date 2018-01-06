945fc40183c4d598303b7957246b0ed242b74102
Fabrice Manhart <fabrice.manhart@gmail.com>
2018-01-06 17:51:35 +0100

    e2e test extended

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,14 +9,22 @@ describe('insight App', () => {
     page = new AppPage();
   });
 
-  it('should filter employee by name1', () => {
-    expect(1).toBe(1);
-  });
-
   it('should filter employee by name', () => {
     page.navigateTo('employees');
     element(by.id('tableFilter')).sendKeys('christian moser');
-    const count = element.all(by.css('mat-table > mat-row')).count();
+    const count = element.all(by.css('mat-row')).count();
     expect(count).toEqual(1);
+  });
+
+  it('should show employee phone number', async () => {
+    await page.navigateTo('employees/moc');
+    expect(await element(by.id('phone-business')).getText()).toBe('+41 44 733 6617');
+    expect(await element(by.id('phone-private')).getText()).toBe('+41 79 261 6814');
+  });
+
+  it('should show employee description in En and De', () => {
+    page.navigateTo('employees/moc');
+    expect(element(by.id('aboutDe')).getText()).toContain('Christian Moser leitet bei Zühlke seit 2014 die Competence Unit .NET');
+    expect(element(by.id('aboutEn')).getText()).toContain('Since 2014 Christian Moser is head of the competence unit .NET with around 40');
   });
 });
