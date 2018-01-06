01f11eadc4b568bd4b3f2628b9a02b26b10c32d8
Fabrice Manhart <fabrice.manhart@gmail.com>
2018-01-06 17:30:27 +0100

    e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,6 @@
+import { by } from 'protractor/built';
 import { AppPage } from './app.po';
+import { element } from 'protractor';
 
 describe('insight App', () => {
   let page: AppPage;
@@ -7,8 +9,14 @@ describe('insight App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should filter employee by name1', () => {
+    expect(1).toBe(1);
+  });
+
+  it('should filter employee by name', () => {
+    page.navigateTo('employees');
+    element(by.id('tableFilter')).sendKeys('christian moser');
+    const count = element.all(by.css('mat-table > mat-row')).count();
+    expect(count).toEqual(1);
   });
 });
